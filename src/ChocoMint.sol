d29a45b2df16590dd906f1fd9db164e64d6beb73
0xmagus <105892146+0xmagus@users.noreply.github.com>
2022-07-02 05:48:03 -0700

    Update to proxy contract implementation.

--- src/ChocoMint.sol
+++ src/ChocoMint.sol
@@ -1,34 +1,54 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "lib/forge-std/src/console.sol";
+import "lib/forge-std/src/console2.sol";
 
 interface IERC721 {
   function balanceOf(address owner) external view returns (uint256);
   function transferFrom(address from, address to, uint256 tokenId) external;
 }
 
-contract TempUser {
-    // Assumes ids in collection are minted in order
-    uint256 startId = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+// Assumes ids in collection are minted in order
+contract ImplementationUser {
+    uint256 private startId;
 
-    function execute(address _target, bytes calldata _payload, address _owner) external payable {
+    function mint(address _target, address payable _owner, bytes calldata _payload) external payable {
+        startId = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
         (bool _success, bytes memory _response) = _target.call{value: msg.value}(_payload);
         require(_success); _response;
-        
         IERC721 token = IERC721(_target);
         uint256 count = token.balanceOf(address(this));
         for (uint256 i = 0; i < count;) {
             token.transferFrom(address(this), _owner, startId+i);
             unchecked {++i;}
         }
+        if (address(this).balance > 0) {
+            _owner.transfer(address(this).balance);
+        } 
+    }
+}
 
-        selfdestruct(payable(_owner));
+contract ProxyUser {
+    uint256 private startId;
+
+    fallback() external payable {
+        assembly {
+            let target := calldataload(0x0)
+            let freememstart := mload(0x40)
+            calldatacopy(freememstart, 32, sub(calldatasize(), 32))
+            let success := delegatecall(gas(), target, freememstart, sub(calldatasize(), 32), 0x40, 32)
+            returndatacopy(0x0, 0x0, returndatasize())
+            switch success case 0 {revert(0, 0)} default {return (0, returndatasize())}
+        }
     }
 
+    receive() external payable{}
+
     function onERC721Received(address, address, uint256 id, bytes calldata) external returns (bytes4) {
-        if (startId == 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff) {
-            startId = id;
+        assembly {
+            if eq(0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff, sload(0)) {
+                sstore(0x0, id)
+            }
         }
         return 0x150b7a02;
     }
@@ -46,18 +66,26 @@ contract ChocoMint {
         owner = msg.sender;
     }
 
+    function createProxy(uint256 amount) external onlyOwner returns (address[] memory) {
+        address[] memory proxyAddresses = new address[](amount);
+        for (uint256 i = 0; i < amount; i++) {
+            proxyAddresses[i] = address(new ProxyUser());
+        }
+        return proxyAddresses;
+    }
+
     function execute(
         uint256 _value, 
-        address _target,
-        bytes calldata _payload, 
-        uint256 loops, 
-        uint256 _targetBlock, 
+        bytes calldata _payload,
+        address[] calldata proxies,
+        uint256 _targetBlock,
         uint256 _toCoinbase
     ) external onlyOwner payable {
         require(_targetBlock == block.number || _targetBlock == 0);
-
+        uint256 loops = proxies.length;
         for (uint256 i = 0; i < loops;) {
-            new TempUser().execute{value: _value}(_target, _payload, owner);
+            (bool _success, bytes memory _response) = proxies[i].call{value: _value}(_payload);
+            require(_success); _response;
             unchecked {++i;}
         }
         if (_toCoinbase > 0) {
--- test/ChocoMint.t.sol
+++ test/ChocoMint.t.sol
@@ -2,23 +2,60 @@
 pragma solidity ^0.8.13;
 
 import "lib/forge-std/src/Test.sol";
-import "lib/forge-std/src/console.sol";
+import "lib/forge-std/src/console2.sol";
 import "../src/ChocoMint.sol";
 
 contract ChocoMintTest is Test {
-    function setUp() public {}
+    Vm public VM;
+
+    function setUp() public {
+        VM = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    }
 
     function testExecute() public {
-        // Test on mint of PieceOfShit NFT collection
-        uint256 mintPerAddress = 2; // mint 2 free per address
-        uint256 value = 0; // free mint no ether in this case
-        address target = 0x9F9B2B8e268d06DC67F0f76627654b80e219e1d6; // pieceOfShit contract
-        bytes memory payload = abi.encodeWithSignature("mint(uint32)", mintPerAddress); 
-        uint256 loops = 100; // mint 100x2 = 200 total
-        uint256 targetBlock = 14879473; // used for flashbots
-        uint256 toCoinbase = 0; // none provided to miner
         ChocoMint chocoMint = new ChocoMint();
-        chocoMint.execute(value, target, payload, loops, targetBlock, toCoinbase);
-        assertEq(IERC721(target).balanceOf(address(this)), 200);
+        address impUser = address(new ImplementationUser());
+        address[] memory proxies = chocoMint.createProxy(100);
+
+        // Test on mint of PieceOfShit NFT collection
+        uint256 value = 0; // free mint no ether in this case
+        address mintTarget = 0x9F9B2B8e268d06DC67F0f76627654b80e219e1d6; // pieceOfShit contract
+        bytes memory mintPayload = abi.encodeWithSignature("mint(uint32)", 2); 
+        bytes memory impPayload = abi.encodeWithSignature(
+            "mint(address,address,bytes)", 
+            mintTarget, 
+            address(this),
+            mintPayload);
+        bytes memory proxyPayload = abi.encodePacked(abi.encode(address(impUser)), impPayload);
+
+        uint256 targetBlock = 14879473; // used for flashbots to prevent uncled tx to get confirmed
+        uint256 toCoinbase = 0; // none provided to miner
+
+        chocoMint.execute(value, proxyPayload, proxies, targetBlock, toCoinbase);
+        assertEq(IERC721(mintTarget).balanceOf(address(this)), 200);
+    }
+
+    function testCreateProxy() public {
+        ChocoMint chocoMint = new ChocoMint();
+        address[] memory proxies = chocoMint.createProxy(1);
+        assertEq(proxies.length, 1);
+    }
+
+    function testProxyUser() public {
+        address impUser = address(new ImplementationUser());
+        address proxy = address(new ProxyUser());
+
+        address mintTarget = 0x9F9B2B8e268d06DC67F0f76627654b80e219e1d6; // pieceOfShit contract
+        bytes memory mintPayload = abi.encodeWithSignature("mint(uint32)", 2); // mint 2
+        bytes memory impPayload = abi.encodeWithSignature(
+            "mint(address,address,bytes)", 
+            mintTarget, 
+            address(this),
+            mintPayload);
+        bytes memory proxyPayload = abi.encodePacked(abi.encode(impUser), impPayload);
+
+        (bool _success, bytes memory _response) = proxy.call(proxyPayload);
+        assertTrue(_success); _response;
+        assertEq(IERC721(mintTarget).balanceOf(address(this)), 2);
     }
 }
